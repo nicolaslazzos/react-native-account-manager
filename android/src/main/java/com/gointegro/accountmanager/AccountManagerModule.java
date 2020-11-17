de4971b182a90be747d242d605d453ac98ec4333
nicolaslazzos <nicolaslazzos09@gmail.com>
2020-11-17 12:15:36 -0300

    setting min api level to 23 and resolving deprecated methods

--- android/src/main/java/com/gointegro/accountmanager/AccountManagerModule.java
+++ android/src/main/java/com/gointegro/accountmanager/AccountManagerModule.java
@@ -1,8 +1,6 @@
 package com.gointegro.accountmanager;
 
-import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
@@ -10,37 +8,36 @@ import com.facebook.react.bridge.WritableNativeArray;
 import com.facebook.react.bridge.WritableNativeMap;
 import com.facebook.react.bridge.ReadableMap;
 
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
 
-import java.io.IOException;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map.Entry;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import android.R.string;
 import android.accounts.Account;
 import android.accounts.AccountManager;
-import android.accounts.AccountManagerFuture;
-import android.accounts.AuthenticatorException;
-import android.accounts.OperationCanceledException;
 import android.os.Bundle;
 
 
 public class AccountManagerModule extends ReactContextBaseJavaModule {
 	private ReactApplicationContext _reactContext = null;
 
+	public static final String MODULE_NAME = "RNAccountManager";
+
+	/** Known error codes. */
+	@interface Errors {
+		String E_ACCOUNT_ALREADY_EXISTS = "E_ACCOUNT_ALREADY_EXISTS";
+		String E_INVALID_ACCOUNT = "E_INVALID_ACCOUNT";
+		String E_REMOVE_FAILED = "E_REMOVE_FAILED";
+	}
+
 	public AccountManagerModule(ReactApplicationContext reactContext) {
-    super(reactContext);
+		super(reactContext);
 		_reactContext = reactContext;
 	}
 
 	@Override
-  public String getName() {
-    return "AccountManager";
-  }
+	public String getName() {
+		return MODULE_NAME;
+	}
 
 	AccountManager manager = null;
 
@@ -64,22 +61,22 @@ public class AccountManagerModule extends ReactContextBaseJavaModule {
 
 	@ReactMethod
 	public void getAccountsByType (String accountType, Promise promise) {
-			manager = AccountManager.get(_reactContext);
-			Account[] account_list = manager.getAccountsByType(accountType);
-			WritableNativeArray result = new WritableNativeArray();
+		manager = AccountManager.get(_reactContext);
+		Account[] account_list = manager.getAccountsByType(accountType);
+		WritableNativeArray result = new WritableNativeArray();
 
-			for(Account account: account_list)
-			{
-				Integer index = indexForAccount(account);
+		for(Account account: account_list)
+		{
+			Integer index = indexForAccount(account);
 
-				WritableNativeMap account_object = new WritableNativeMap();
-				account_object.putInt("_index", (int)index);
-				account_object.putString("name", account.name);
-				account_object.putString("type", account.type);
-				result.pushMap(account_object);
-			}
+			WritableNativeMap account_object = new WritableNativeMap();
+			account_object.putInt("_index", (int)index);
+			account_object.putString("name", account.name);
+			account_object.putString("type", account.type);
+			result.pushMap(account_object);
+		}
 
-			promise.resolve(result);
+		promise.resolve(result);
 	}
 
 	@ReactMethod
@@ -90,7 +87,7 @@ public class AccountManagerModule extends ReactContextBaseJavaModule {
 		Bundle userdata = new Bundle();
 
 		if(false == manager.addAccountExplicitly(account, password, userdata)){
-			promise.reject("Account with username already exists!");
+			promise.reject(Errors.E_ACCOUNT_ALREADY_EXISTS, new Error("Account with username already exists!"));
 			return;
 		}
 
@@ -100,63 +97,52 @@ public class AccountManagerModule extends ReactContextBaseJavaModule {
 		result.putString("type", account.type);
 
 		promise.resolve(result);
-		return;
 	}
 
 	@ReactMethod
 	public void removeAccount (ReadableMap accountObject, Promise promise) {
 		int index = accountObject.getInt("_index");
 		Account account = accounts.get(index);
+
 		if(account == null){
-			promise.reject("Invalid account");
+			promise.reject(Errors.E_INVALID_ACCOUNT, new Error("Invalid account"));
 			return;
 		}
 
-		AccountManagerFuture<Boolean> future = manager.removeAccount(account, null, null);
-		try {
-			if(future.getResult() == true) {
-				accounts.remove(index);
-				promise.resolve(new WritableNativeMap());
-			} else {
-				promise.reject("Failed to remove account");
-			}
-		}
-		catch (OperationCanceledException e) {
-			promise.reject("Operation canceled: " + e.getLocalizedMessage());
-		}
-		catch (AuthenticatorException e) {
-			promise.reject("Authenticator error: " + e.getLocalizedMessage());
-		}
-		catch (IOException e) {
-			promise.reject("IO error: " + e.getLocalizedMessage());
-		}
+		Boolean success = manager.removeAccountExplicitly(account);
 
-		return;
+		if(success == true) {
+			accounts.remove(index);
+			promise.resolve(new WritableNativeMap());
+		} else {
+			promise.reject(Errors.E_REMOVE_FAILED, new Error("Failed to remove account"));
+		}
 	}
 
 	@ReactMethod
 	public void setUserData (ReadableMap accountObject, String key, String data, Promise promise) {
 		Account account = accounts.get(accountObject.getInt("_index"));
+
 		if(account == null) {
-			promise.reject("Invalid account");
+			promise.reject(Errors.E_INVALID_ACCOUNT, new Error("Invalid account"));
 			return;
 		}
+
 		manager.setUserData(account, key, data);
 		promise.resolve(new WritableNativeMap());
-		return;
 	}
 
 	@ReactMethod
 	public void getUserData (ReadableMap accountObject, String key, Promise promise) {
 		Account account = accounts.get(accountObject.getInt("_index"));
+
 		if(account == null) {
-			promise.reject("Invalid account");
+			promise.reject(Errors.E_INVALID_ACCOUNT, new Error("Invalid account"));
 			return;
 		}
 
 		WritableNativeMap result = new WritableNativeMap();
 		result.putString("value", manager.getUserData(account, key));
 		promise.resolve(result);
-		return;
 	}
 }
